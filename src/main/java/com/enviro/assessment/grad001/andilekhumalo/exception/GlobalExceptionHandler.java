2bf323364fb3506974800fa1c85492dca636da73
Andile Khumalo <akhumalo023a@student.wethinkcode.co.za>
2024-07-02 15:10:26 +0200

    Created a record class for displaying any internal errors that may occur in json format.

--- src/main/java/com/enviro/assessment/grad001/andilekhumalo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enviro/assessment/grad001/andilekhumalo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.enviro.assessment.grad001.andilekhumalo.exception;
 
+import com.enviro.assessment.grad001.andilekhumalo.exception.record.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,12 +10,14 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleGeneralExceptions(Exception ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<ErrorResponse> handleGeneralExceptions(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<Object> handleNotFoundException(NotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 }
---
+++ src/main/java/com/enviro/assessment/grad001/andilekhumalo/exception/record/ErrorResponse.java
@@ -0,0 +1,4 @@
+package com.enviro.assessment.grad001.andilekhumalo.exception.record;
+
+public record ErrorResponse(String message) {
+}
